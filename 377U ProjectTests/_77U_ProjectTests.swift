6c2a0cbdbdc1fa217550556ac2c8a9361dcbbd7d
Samuel Hinshelwood Jr <samhinsh@gmail.com>
2016-04-23 17:21:51 -0700

    Initial Commit

---
+++ 377U ProjectTests/_77U_ProjectTests.swift
@@ -0,0 +1,36 @@
+//
+//  _77U_ProjectTests.swift
+//  377U ProjectTests
+//
+//  Created by Samuel Hinshelwood on 4/23/16.
+//  Copyright © 2016 Samuel Hinshelwood. All rights reserved.
+//
+
+import XCTest
+@testable import _77U_Project
+
+class _77U_ProjectTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
