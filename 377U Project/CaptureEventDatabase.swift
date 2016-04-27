7cf091affb529452c7c87ce0e975c778ef88d9b6
Samuel Hinshelwood Jr <samhinsh@gmail.com>
2016-04-27 01:49:30 -0700

    Wrote JSON data collection

--- 377U Project/CaptureEventDatabase.swift
+++ 377U Project/CaptureEventDatabase.swift
@@ -13,51 +13,17 @@ class CaptureEventDatabase {
     // CaptureEvents
     var allCreatedCaptureEvents: [CaptureEvent]? // TODO: priority queue based on distance from location
     
-    // TODO: Use iOS JSON parser
-    /*
-    private var hardCodeDataBase = {
-        "events" : [
-            {
-            "title" : "Black lives matter",
-            "about" : "#BlackLivesMatter was created in 2012 after Trayvon Martin’s murderer, George Zimmerman, was acquitted for his crime, and dead 17-year old Trayvon was post-humously placed on trial for his own murder. Rooted in the experiences of Black people in this country who actively resist our de-humanization, #BlackLivesMatter is a call to action and a response to the virulent anti-Black racism that permeates our society.Black Lives Matter is a unique contribution that goes beyond extrajudicial killings of Black people by police and vigilantes.",
-            "coordinates" : "37.424428, -122.169929",
-            "media" : "blm1.jpg, blm2.jpg.blm3.jpg,blm4.jpg,blm5.jpg,blm6.jpg,blm7.jpg,blm8.jpg,blm9.jpg",
-            "hashtag" : "#blacklivesmatter"
-            },
+    private func parseJSONIntoCaptureEvents() -> [CaptureEvent] {
+        do {
+            //let json = try NSJ
+            // TODO
+            return [CaptureEvent]()
+        } catch {
             
-            {
-            "title" : "Who's Teaching Us",
-            "about" : "Who’s Teaching Us? raises awareness on the need for faculty diversity and support for marginalized studies and community centers on campus.",
-            "coordinates" : "37.429492, -122.169581",
-            "media" : "wtu1.jpg,wtu2.jpg,wtu3.jpg,wtu4.jpg,wtu5.jpg,wtu6.jpg,wtu7.jpg,wtu8.jpg",
-            "hashtag" : "#wtu"
-            },
-            
-            {
-            "title" : "Fossil free Stanford",
-            "about" : "Student movement calling on Stanford University to freeze new fossil fuel investments and fully divest from fossil fuels within the next five years.",
-            "coordinates" : "37.427554, -122.169357",
-            "media" : "dfc1.jpg,dfc2.jpg,dfc3.jpg,dfc4.jpg,dfc5.jpg,dfc6.jpg,dfc7.jpg,dfc8.jpg",
-            "hashtag" : "#fossilfreestanford"
-            },
-            
-            {
-            "title" : "Take back the Night",
-            "about" : "A rally, a march, a speakout about sexual violence on campus",
-            "coordinates" : "37.424428, -122.169929",
-            "media" : "tbn1.jpg,tbn2.jpg,tbn3.jpg,tbn4.jpg,tbn5.jpg,tbn6.jpg,tbn7.jpg,tbn8.jpg",
-            "hashtag" : "#takebackthenight"
-            },
-            
-            {
-            "title" : "Out of the Darkness",
-            "about" : "Out of the Darkness walks are approximately 5k walk events that fundraise and create awareness for suicide prevention and serve as a place for survivors of suicide loss and those suffering from mental illness to come together in support of continued suicide prevention and mental health education, programming, and legislation.",
-            "coordinates" : "37.4320965,-122.1605481",
-            "media" : "ood1.jpg,ood2.jpg,ood3.jpg,ood4.jpg,ood5.jpg,ood6.jpg,ood7.jpg",
-            "hashtag" : "#outofdarkness"
-            }
-            
-            ]
         }
-        */
+    }
+    
+    func fetchCaptureEvents() {
+        allCreatedCaptureEvents = parseJSONIntoCaptureEvents()
+    }
 }
