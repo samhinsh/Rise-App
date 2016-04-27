1b541579db069b230e1c9e9935e27b02645bf97d
Samuel Hinshelwood Jr <samhinsh@gmail.com>
2016-04-26 20:29:21 -0700

    Added camera screen, annotated single pin

---
+++ 377U Project/CameraView.swift
@@ -0,0 +1,24 @@
+//
+//  CameraButtonView.swift
+//  377U Project
+//
+//  Created by Samuel Hinshelwood on 4/26/16.
+//  Copyright © 2016 Samuel Hinshelwood. All rights reserved.
+//
+
+import UIKit
+
+class CameraView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    // methods for drawing camera icons and such
+    // potentially create another class for camera button itself
+
+}
--- 377U Project/StartScreenViewController.swift
+++ 377U Project/StartScreenViewController.swift
@@ -10,23 +10,54 @@ import UIKit
 import MapKit
 
 class StartScreenViewController: UIViewController, MKMapViewDelegate {
-
-    @IBOutlet weak var mapView: MKMapView!
+    
+    /* expose revelant location */
+    @IBOutlet private weak var mapView: MKMapView!
+    
+    /* expose relevant capture events */
+    @IBOutlet private weak var captureEventsTable: UITableView!
+    
+    /* Camera button for IU*/
+    @IBOutlet weak var cameraButton: CameraView!
+    var point: MKPointAnnotation = MKPointAnnotation()
+    
+    /* Selector showing 'nearest' or 'trending' CaptureEvents has changed
+     * Display appropriate CaptureEvents in the table */
+    @IBAction private func captureEventDisplayChanged(sender: UISegmentedControl)
+    {
+        
+        switch sender.selectedSegmentIndex {
+        case 0:
+            print("Displaying events nearest to you")
+            // displayNearbyCaptureEvents()
+            
+        case 1:
+            print("Displaying trending events")
+            // displayTrendingCaptureEvents()
+            
+        default: break;
+        }
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // display Map w/ user location
         
-        // display Map
-        
-        // display available nearby CaptureEvent's
+        // display available nearby CaptureEvents
+        point.coordinate = CLLocationCoordinate2D(latitude: 37.429492,
+                                                  longitude: -122.169581)
+        point.title = "Who's Teaching Us Rally"
+        point.subtitle = "Garner equitable education"
+        mapView.addAnnotation(point)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
